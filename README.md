1efcfed159f53fcad4ec4f029fe920ef05f90bfd
Brian Mathews <brian.mathews@formidable.com>
2017-05-22 07:42:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Eject create-react-app with Preact.
+
+###
+npm run start
+
+###
+npm run build
---
+++ src/jsfeat.js
@@ -0,0 +1,6 @@
+const jsfeat = require("jsfeat");
+window.jsfeat = jsfeat;
+
+require("jsfeat/cascades/frontalface.js");
+
+export default jsfeat;
